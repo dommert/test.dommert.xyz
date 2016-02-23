98968d0b1750c447fe235a8cd48d9929c45a7686
Dommert <DommertEnterprises@Gmail.com>
2016-02-22 17:02:30 -0700

    auth

--- FlaskApp/auth.py
+++ FlaskApp/auth.py
@@ -6,6 +6,7 @@ from flask.ext.security import Security, PeeweeUserDatastore, \
 
 # Create app
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['DATABASE'] = {
