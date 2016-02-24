baf499217c1623fee41fd045e50805002f341561
Dommert <DommertEnterprises@Gmail.com>
2016-02-23 20:29:35 -0700

    config updates

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,16 +1,25 @@
-#
-# Flask Configs
-DEBUG = True
-PORT = 80
-HOST = "0.0.0.0"
+# Dommert Flask Configs
+# Configs v0.1.0-alpha
 
 
-SECRET_KEY = "s0me random string!!"
-PASSWORD_SALT = "some0th3r Rad0m String!11"
-TOKEN_SECRET = "S0m3Thing R4nd0m Str!ng1!"
 
-# PeeWee DB Config
 class Configuration(object):
+# Flask Configs
+    DEBUG = True
+    PORT = 80 # default: 5000
+    HOST = "0.0.0.0"
+    STATIC = 'static'
+    TEMPLATE = 'templates'
+
+# Secrets, Keys, and Salts
+    # Flask
+    SECRET_KEY = "s0me random string!!"
+    # Auth
+    PASSWORD_SALT = "some0th3r Rad0m String!11"
+    # JWT
+    TOKEN_SECRET = "S0m3Thing R4nd0m Str!ng1!"
+
+# PeeWee Database
     DATABASE = {
         'name': 'flask_netbook.db',
         'engine': 'peewee.SqliteDatabase',
