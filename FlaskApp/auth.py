0be9eed707a7154290107fd21cef3c205a2a8da1
Dommert <DommertEnterprises@Gmail.com>
2016-02-24 20:23:58 -0700

    auth and models

---
+++ FlaskApp/auth.py
@@ -0,0 +1,116 @@
+# Auth
+
+from flask import Flask
+from app import app
+
+
+
+# Create Users
+
+
+# Login
+
+
+# LogOut
+
+
+# Hash_Pass
+
+
+# Verify_Hash
+
+
+# Create Token
+def create_token(user):
+    payload = {
+        # subject
+        'sub': user.id,
+        #issued at
+        'iat': datetime.utcnow(),
+        #expiry
+        'exp': datetime.utcnow() + timedelta(days=1)
+    }
+
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token.decode('unicode_escape')
+
+
+
+# Parse Token
+def parse_token(req):
+    token = req.headers.get('Authorization').split()[1]
+    return jwt.decode(token, SECRET_KEY, algorithms='HS256')
+
+# @Token
+
+def token(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not request.headers.get('Authorization'):
+            response = jsonify(message='Missing authorization header')
+            response.status_code = 401
+            return response
+        try:
+            payload = parse_token(request)
+        except DecodeError:
+            response = jsonify(message='Token is invalid')
+            response.status_code = 401
+            return response
+        except ExpiredSignature:
+            response = jsonify(message='Token has expired')
+            response.status_code = 401
+            return response
+
+        g.user_id = payload['sub']
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+
+# ------ ROUTES -------- #
+
+# Sign Up
+@app.route('/api/register', methods=['POST'])
+def register():
+    json_data = request.json
+    # JSON Data
+    user = User(
+        email=json_data['email'],
+        password=json_data['password']
+    )
+    try:
+        # Add User
+        db.session.add(user)
+        db.session.commit()
+        status = 'success'
+    except:
+        status = 'this user is already registered'
+    db.session.close()
+    return jsonify({'result': status})
+
+# LOGIN
+@app.route('/api/login', methods=['POST'])
+def login():
+    json_data = request.json
+    # Find Email
+    user = User.query.filter_by(email=json_data['email']).first()
+    if user and bcrypt.check_password_hash(
+            user.password, json_data['password']):
+        session['logged_in'] = True
+        status = True
+    else:
+        status = False
+    return jsonify({'result': status})
+
+
+
+@app.route('/api/dummy-api/', methods=['GET'])
+@auth_token_required
+def dummyAPI():
+    ret_dict = {
+        "Key1": "Value1",
+        "Key2": "value2"
+    }
+    return jsonify(items=ret_dict)
---
+++ FlaskApp/model.py
@@ -0,0 +1,27 @@
+# Model py
+import datetime
+from app import db
+from peewee import *
+from flask_chairy.util import make_password, check_password
+
+
+# -----------------------------------------------------
+
+class BaseUser(object):
+    def set_password(self, password):
+        self.password = make_password(password)
+
+    def check_password(self, password):
+        return check_password(password, self.password)
+
+# User Class
+class User(db.Model, BaseUser):
+    username = CharField(unique=True)
+    password = CharField()
+    email = CharField(unique=True)
+    join_date = DateTimeField(default=datetime.datetime.now)
+    active = BooleanField(default=True)
+    admin = BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.username
